f281171e240dc39f405489951f79e54bac5463d3
cristina1084 <cristinajohns@cs.ajce.in>
2019-07-24 15:28:01 +0530

    entry loops

---
+++ htmlcss/js/forloop.js
@@ -0,0 +1,7 @@
+//Entry controlled loop
+
+//for(initialization; condition; updation) {...statements...}
+
+for(var i=1; i<=10; i++){
+    console.log(i)
+}
---
+++ htmlcss/js/whileloop.js
@@ -0,0 +1,8 @@
+//Entry controlled loop 
+
+var x = 1;  //initialization
+
+while (x <= 10){     //condition
+    console.log(x)
+    x++;          //updation
+}
