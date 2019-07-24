751062bdf5186527e2778f8142aaa2f84606af9d
cristina1084 <cristinajohns@cs.ajce.in>
2019-07-24 14:45:10 +0530

    javascript

--- htmlcss/js/add.js
+++ htmlcss/js/add.js
@@ -1,3 +1,7 @@
+/*node filename.js
+var,let,const 
+=== data type checking */
+
 var x=5;
 var y=10;
 var z=x+y;
---
+++ htmlcss/js/even.js
@@ -0,0 +1,10 @@
+/*check whether number is even or odd*/
+
+var x =985;
+
+if (x%2 == 0){
+    console.log("Even")
+}
+else{
+    console.log("Odd")
+}
