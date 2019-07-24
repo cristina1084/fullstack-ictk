6d080f443c3ea48b848d42828646e12a4c98dce9
cristina1084 <cristinajohns@cs.ajce.in>
2019-07-24 15:10:27 +0530

    added comments

--- htmlcss/js/test.js
+++ htmlcss/js/test.js
@@ -1,9 +1,12 @@
 var x=10; //global scope
 const y=12; //constant assignment, local scope
+//y=y+1 -> error
 if (x<15){
     let x=16;  //local scope
     x=x+1;
+    const a = 13;
     console.log(x)
     console.log(y)
 }
 console.log(x)
+// console.log(a) -> error "a is not defined"
