6ab2d3b1c40f06d696eb8fa51548133e9a93a6ef
cristina1084 <cristinajohns@cs.ajce.in>
2019-07-29 14:24:06 +0530

    jquery 1.0

--- htmlcss/js/jqueryeg.js
+++ htmlcss/js/jqueryeg.js
@@ -1,12 +1,20 @@
 //jQuery
 
-$(document).ready( function () {     //document refers to DOM, ready function checks whether DOM has loaded
+/* $(document).ready( function () {     //document refers to DOM, ready function checks whether DOM has loaded
     $("h1").click( function (){
         //alert("Clicked");
         $(this).hide();           //'this' points to the current object
     });
-});
+}); */
 
+$(document).ready( function(){
+    $('#btn1').click(function(){
+        $("#p1").show();
+    });
+    $('#btn2').click(function(){
+        $('#p1').hide();
+    });
+});
 
 //<h1> --->  $("h1")
 //<h1 class="test">  --> $(".test")
